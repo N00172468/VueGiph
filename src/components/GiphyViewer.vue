4f2a386e10fa41efcb9636c23f78e8bbcf228706
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-01-28 14:33:00 +0000

    Added search bar

--- src/components/GiphyViewer.vue
+++ src/components/GiphyViewer.vue
@@ -1,15 +1,22 @@
 <template>
-    <b-card-group columns>
-        <b-card
-            v-for="gif in gifs"
-            :key="gif.id"
-            :img-src="gif.images.fixed_width.url"
-            :img-alt="gif.title">
-            <b-card-text>
-                <a href="gif.url" target="_blank">{{ gif.title }}</a>
-            </b-card-text>
-        </b-card>
-    </b-card-group>
+    <div>
+        <div class="search-box">
+            <input type="text" v-model="term" v-on:keyup.enter="searchGiphy()" />
+            <b-button class="float-right" variant="primary" @click="searchGiphy()">Search</b-button>
+        </div>
+
+        <b-card-group columns>
+            <b-card
+                v-for="gif in gifs"
+                :key="gif.id"
+                :img-src="gif.images.fixed_width.url"
+                :img-alt="gif.title">
+                <b-card-text>
+                    <a href="gif.url" target="_blank">{{ gif.title }}</a>
+                </b-card-text>
+            </b-card>
+        </b-card-group>
+    </div>
 </template>
 
 <script>
@@ -37,7 +44,26 @@ export default {
         .catch(error => console.log(error))
     },
     methods: {
-
+        searchGiphy() {
+            if (!this.term) {
+                alert("Enter the search term s'il vous plait!");
+                return;
+            }
+            axios.get(`${GIPHY_URL}/search?api_key=${API_KEY}&q={this.term}&limit=20`)
+            .then(response => (
+                this.gifs = response.data.data
+            ))
+            .catch(error => console.log(error))
+            
+            this.term = ""
+        }
     }
 };
 </script>
+
+<style>
+.search-box {
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+</style>
