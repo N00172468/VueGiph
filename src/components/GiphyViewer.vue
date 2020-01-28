77a80cd77832f1ed7f86006690e8a474efedcc84
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-01-28 14:03:41 +0000

    Added API

--- src/components/GiphyViewer.vue
+++ src/components/GiphyViewer.vue
@@ -6,7 +6,11 @@
 
 <script>
 /* eslint-disable no-console */
- 
+import axios from 'axios'
+
+const GIPHY_URL = "https://api.giphy.com/v1/gifs";
+const API_KEY = "c5uhkIh6od7M07RcwDFKiT7tzdTB5CEB";
+
 export default {
     name: "GiphyViewer",
     components: {
@@ -14,8 +18,15 @@ export default {
     },
     data() {
         return {
-
+            gifs: []
         };
+    },
+    mounted() {
+        axios.get(`${GIPHY_URL}/trending?api_key=${API_KEY}`)
+        .then(response => (
+            this.gifs = response.data.data
+        ))
+        .catch(error => console.log(error))
     },
     methods: {
 
